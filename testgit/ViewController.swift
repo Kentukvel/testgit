995ae7e1d81c5e69cc188c30c09750a130fdabb0
dimonicle <dimonicle33@gmail.com>
2018-12-09 14:45:36 +0200

    Initial Commit

---
+++ testgit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testgit
+//
+//  Created by Левкутник Дмитрий on 09.12.2018.
+//  Copyright © 2018 Левкутник Дмитрий. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
